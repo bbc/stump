2ce4a235afad67e17a957de9d2ab51e128cd7f0d
Joseph Pack <josephapack@hotmail.co.uk>
2019-03-06 17:27:38 +0000

    Single generic entry point to module

--- lib/event_logger.ex
+++ lib/event_logger.ex
@@ -1,39 +1,52 @@
 defmodule EventLogger do
   import Logger, only: [info: 1, warn: 1, error: 1]
+  import String, only: [downcase: 1]
 
-  def info(data) when is_map(data) do
-    Map.merge(data, %{datetime: DateTime.utc_now, level: "info"})
-    |> Poison.encode!()
+  def log(level, data) do
+    case downcase(level) do
+      "info"  -> info(level, data)
+      "warn"  -> warn(level, data)
+      "error" -> error(level, data)
+    end
+  end
+
+  defp info(level, data) when is_map(data) do
+    map(level, data)
     |> Logger.info
   end
 
-  def info(data) when is_bitstring(data) do
-    %{message: data, datetime: DateTime.utc_now, level: "info"}
-    |> Poison.encode!()
+  defp info(level, data) when is_bitstring(data) do
+    string(level, data)
     |> Logger.info
   end
 
-  def warn(data) when is_map(data) do
-    Map.merge(data, %{datetime: DateTime.utc_now, level: "warn"})
-    |> Poison.encode!()
+  defp warn(level, data) when is_map(data) do
+    map(level, data)
     |> Logger.warn
   end
 
-  def warn(data) when is_bitstring(data) do
-    %{message: data, datetime: DateTime.utc_now, level: "warn"}
-    |> Poison.encode!()
+  defp warn(level, data) when is_bitstring(data) do
+    string(level, data)
     |> Logger.warn
   end
 
-  def error(data) when is_map(data) do
-    Map.merge(data, %{datetime: DateTIme.utc_now, level: "error"})
-    |> Poison.encode!()
+  defp error(level, data) when is_map(data) do
+    map(level, data)
     |> Logger.error
   end
 
-  def error(data) when is_bitstring(data) do
-    %{message: data, datetime: DateTime.utc_now, level: "error"}
-    |> Poison.encode!()
+  defp error(level, data) when is_bitstring(data) do
+    string(level, data)
     |> Logger.error
+  end
+
+  defp map(level, data) do
+    Map.merge(data, %{datetime: DateTime.utc_now, level: to_string(level)})
+    |> Poison.encode!()
+  end
+
+  defp string(level, data) do
+    %{message: data, datetime: DateTime.utc_now, level: to_string(level)}
+    |> Poison.encode!()
   end
 end

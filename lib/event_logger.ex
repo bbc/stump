6b488e2cbe32ddc2df0322da7021cd55cc263913
Joseph Pack <josephapack@hotmail.co.uk>
2019-03-06 14:48:10 +0000

    Added EventLogger.info function

--- lib/event_logger.ex
+++ lib/event_logger.ex
@@ -1,2 +1,15 @@
 defmodule EventLogger do
+  import Logger, only: [info: 1, warn: 1, error: 1]
+
+  def info(data) when is_map(data) do
+    Map.merge(data, %{datetime: DateTime.utc_now, level: "info"})
+    |> Poison.encode!()
+    |> Logger.inf
+  end
+
+  def info(data) when is_bitstring(data) do
+    %{message: data, datetime: DateTime.utc_now, level: "info"}
+    |> Poison.encode!()
+    |> Logger.info
+  end
 end

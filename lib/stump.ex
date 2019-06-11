83a8d124430183adf00f329cd8504189d492fc73
Joseph Pack <josephapack@hotmail.co.uk>
2019-06-11 14:45:23 +0100

    Use non ! Jason.encode to save parent app
    When using the ! for both Jason and Poison .encode! the error would be
    raised causing the Logger to fail, the result of this was that the
    parent app using Stump could crash. This change should solve that
    problem ensuring we dont forcibly raise the exception but instead log
    the error message we receive when trying to encode it.

--- lib/stump.ex
+++ lib/stump.ex
@@ -35,12 +35,20 @@ defmodule Stump do
   defp format(level, data) when is_map(data) do
     data
     |> Map.merge(%{datetime: time(), level: to_string(level)})
-    |> Jason.encode!()
+    |> encode()
   end
 
   defp format(level, data) when is_bitstring(data) or is_binary(data) do
     %{message: data, datetime: time(), level: to_string(level)}
-    |> Jason.encode!()
+    |> encode()
+  end
+
+  defp encode(map) do
+    case Jason.encode(map) do
+      {:ok, value}    -> value
+      {:error, value} ->
+        encode(%{message: "There was an error encoding your log message: #{value.message}", datetime: time()})
+    end
   end
 
   @doc false
--- test/event_logger_test.exs
+++ test/event_logger_test.exs
@@ -7,23 +7,35 @@ defmodule StumpTest do
     assert Stump.time == Stump.Time.MockTime.utc_now()
   end
 
-  test "when log level is valid but the message provided is '', it logs an error" do
-    assert capture_log(fn ->Stump.log(:error, "") end) == "{\"datetime\":\"2019-03-01T00:00:00Z\",\"level\":\"error\",\"message\":\"Event Logger received log level, but no error message was provided\"}\n"
+  describe "sucess" do
+    test "when log level is valid but the message provided is '', it logs an error" do
+      assert capture_log(fn ->Stump.log(:error, "") end) == "{\"datetime\":\"2019-03-01T00:00:00Z\",\"level\":\"error\",\"message\":\"Event Logger received log level, but no error message was provided\"}\n"
+    end
+
+    test "when log level is valid but the message provided is nil, it logs an error" do
+      assert capture_log(fn ->Stump.log(:error, nil) end) == "{\"datetime\":\"2019-03-01T00:00:00Z\",\"level\":\"error\",\"message\":\"Event Logger received log level, but no error message was provided\"}\n"
+    end
+
+    test "when log level is :info and a message is provided it, it logs as JSON" do
+      assert capture_log(fn ->Stump.log(:info, "Here is some info") end) == "{\"datetime\":\"2019-03-01T00:00:00Z\",\"level\":\"info\",\"message\":\"Here is some info\"}\n"
+    end
+
+    test "when log level is :warn and a message is provided it, it logs as JSON" do
+      assert capture_log(fn ->Stump.log(:warn, "This is a warning") end) == "{\"datetime\":\"2019-03-01T00:00:00Z\",\"level\":\"warn\",\"message\":\"This is a warning\"}\n"
+    end
+
+    test "when log level is :error and a message is provided it, it logs as JSON" do
+      assert capture_log(fn ->Stump.log(:error, "There was an error") end) == "{\"datetime\":\"2019-03-01T00:00:00Z\",\"level\":\"error\",\"message\":\"There was an error\"}\n"
+    end
   end
 
-  test "when log level is valid but the message provided is nil, it logs an error" do
-    assert capture_log(fn ->Stump.log(:error, nil) end) == "{\"datetime\":\"2019-03-01T00:00:00Z\",\"level\":\"error\",\"message\":\"Event Logger received log level, but no error message was provided\"}\n"
-  end
+  describe "failure" do
+    test "when Stump receives data it cannot encode, it logs the error" do
+      assert capture_log(fn ->Stump.log(:error, <<0x80>>) end) == "{\"datetime\":\"2019-03-01T00:00:00Z\",\"message\":\"There was an error encoding your log message: invalid byte 0x80 in <<128>>\"}\n"
+    end
 
-  test "when log level is :info and a message is provided it, it logs as JSON" do
-    assert capture_log(fn ->Stump.log(:info, "Here is some info") end) == "{\"datetime\":\"2019-03-01T00:00:00Z\",\"level\":\"info\",\"message\":\"Here is some info\"}\n"
-  end
-
-  test "when log level is :warn and a message is provided it, it logs as JSON" do
-    assert capture_log(fn ->Stump.log(:warn, "This is a warning") end) == "{\"datetime\":\"2019-03-01T00:00:00Z\",\"level\":\"warn\",\"message\":\"This is a warning\"}\n"
-  end
-
-  test "when log level is :error and a message is provided it, it logs as JSON" do
-    assert capture_log(fn ->Stump.log(:error, "There was an error") end) == "{\"datetime\":\"2019-03-01T00:00:00Z\",\"level\":\"error\",\"message\":\"There was an error\"}\n"
+    test "when Stump receives a map containing data it cannot encode, it logs the error" do
+      assert capture_log(fn ->Stump.log(:error, %{message: <<0x80>>}) end) == "{\"datetime\":\"2019-03-01T00:00:00Z\",\"message\":\"There was an error encoding your log message: invalid byte 0x80 in <<128>>\"}\n"
+    end
   end
 end
